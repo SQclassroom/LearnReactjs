907601f5811eacfc3885ee7c548ad10e689c2d3a
SQclassroom <sq.vivek@gmail.com>
2023-06-19 13:53:17 +0530

    react1906

---
+++ to-do/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './pages/header/header'
+import Footer from './components/footer'
+import Lists from './components/Page'
+
+const App = () => {
+  return (
+    <div>
+      <Header></Header>
+      <Lists></Lists>
+      <Footer></Footer>
+    </div>
+  )
+}
+
+export default App
--- to-do/src/index.js
+++ to-do/src/index.js
@@ -3,15 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Lists from './components/Page'
-import Footer from './components/footer'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App/>
-    <Lists/>
-    <Footer/>
   </React.StrictMode>
 );
 
